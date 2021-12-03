144e6b3b2f0acd566274f5d8a07f4af4de5ece1c
Gnabriel <elleb@kth.se>
2021-12-03 15:33:16 +0100

    Added structural components for browse view

--- src/components/common/Navbar/NavbarView.jsx
+++ src/components/common/Navbar/NavbarView.jsx
@@ -4,10 +4,10 @@ const NavbarView = () => {
       <div className="px-5 py-2 h-full flex justify-between items-center">
         <span>BWC</span>
         <div className="mr-8 flex gap-14 text-gray-200">
-          <a href="">Home</a>
-          <a href="">Browse</a>
-          <a href="">Marketing</a>
-          <a href="">Login</a>
+          <a href="#undefined">Home</a>
+          <a href="#undefined">Browse</a>
+          <a href="#undefined">Marketing</a>
+          <a href="#undefined">Login</a>
         </div>
       </div>
     </div>
