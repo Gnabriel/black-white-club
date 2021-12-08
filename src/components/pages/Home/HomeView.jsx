e0ef86e7614eb43aed708dae33f8051d1b8a13e3
Gnabriel <elleb@kth.se>
2021-12-08 15:53:31 +0100

    Changed text styling

--- src/components/pages/Home/HomeView.jsx
+++ src/components/pages/Home/HomeView.jsx
@@ -10,8 +10,8 @@ const HomeView = () => {
             <span className="mr-8 absolute right-1/2 text-black">BLACK</span>
             <span className="ml-2 absolute left-1/2 text-white">WHITE</span>
           </span>
-          <span className="mt-0 h-24 font-sans text-6xl tracking-normal">
-            <span className="mr-0 absolute right-1/2 text-black">CL</span>
+          <span className="-mt-0 h-24 font-display text-8xl tracking-normal">
+            <span className="mr-2 absolute right-1/2 text-black">CL</span>
             <span className="ml-0 absolute left-1/2 text-white">UB</span>
           </span>
         </h1>
