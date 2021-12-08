297bba5f2abf56b832e0960194eb7547a55ceced
liteti <61788436+liteti@users.noreply.github.com>
2021-12-08 15:57:11 +0100

    Added postForm

--- src/components/pages/Marketing/MarketingPresenter.jsx
+++ src/components/pages/Marketing/MarketingPresenter.jsx
@@ -1,11 +1,13 @@
 import { Fragment } from "react";
 import NavbarView from "../../common/Navbar/NavbarView";
 import MarketingView from "./MarketingView";
+import PostFormView from "./PostForm/PostFormView";
 
 const MarketingPresenter = () => {
   return (
     <Fragment>
       <NavbarView />
+      <PostFormView />
       <MarketingView />
     </Fragment>
   );
---
+++ src/components/pages/Marketing/PostForm/PostFormView.jsx
@@ -0,0 +1,108 @@
+const PostFormView = () => {
+  return (
+    <div className="mt-10 sm:mt-0">
+      <div className="md:grid md:grid-cols-3 md:gap-6">
+        <div className="md:col-span-1">
+          <div className="px-4 sm:px-0">
+            <h3 className="text-lg font-medium leading-6 text-gray-900">
+              Post a post
+            </h3>
+          </div>
+        </div>
+        <div className="mt-5 md:mt-0 md:col-span-2">
+          <form action="#" method="POST">
+            <div className="shadow overflow-hidden sm:rounded-md">
+              <div className="px-4 py-5 bg-white sm:p-6">
+                <div className="grid grid-cols-6 gap-6">
+                  <div className="col-span-6 sm:col-span-3">
+                    <label
+                      htmlFor="first-name"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Name
+                    </label>
+                    <input
+                      type="text"
+                      name="first-name"
+                      id="first-name"
+                      autoComplete="given-name"
+                      className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                    />
+                  </div>
+
+                  <div className="col-span-6 sm:col-span-3">
+                    <label
+                      htmlFor="email-address"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Email address
+                    </label>
+                    <input
+                      type="text"
+                      name="email-address"
+                      id="email-address"
+                      autoComplete="email"
+                      className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="about"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Text
+                    </label>
+                    <div className="mt-1">
+                      <textarea
+                        id="about"
+                        name="about"
+                        rows={3}
+                        className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
+                        placeholder="What's on your mind?"
+                        defaultValue={""}
+                      />
+                    </div>
+                  </div>
+                  <div className="col-span-6 sm:col-span-3">
+                    <label className="block text-sm font-medium text-gray-700">
+                      Tweet ID
+                    </label>
+                    <input
+                      type="text"
+                      name="tweet-id"
+                      id="tweet-id"
+                      autoComplete="given-name"
+                      placeholder="Number on the end of the tweets URL"
+                      className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                    />
+                  </div>
+                  <div className="col-span-6 sm:col-span-3">
+                    <label className="block text-sm font-medium text-gray-700">
+                      Title
+                    </label>
+                    <input
+                      type="text"
+                      name="title"
+                      id="title"
+                      autoComplete="given-name"
+                      className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                <button
+                  type="submit"
+                  className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Post
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default PostFormView;
