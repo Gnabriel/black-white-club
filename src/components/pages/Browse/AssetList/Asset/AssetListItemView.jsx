29325d89f4ce3406afbd5fc25fc2a495130cc666
Gnabriel <elleb@kth.se>
2021-12-08 10:53:04 +0100

    Fixed loading animation

--- src/components/pages/Browse/AssetList/Asset/AssetListItemView.jsx
+++ src/components/pages/Browse/AssetList/Asset/AssetListItemView.jsx
@@ -1,7 +1,10 @@
 const AssetListItemView = ({ asset, handleClick, isLoading }) => {
   return (
     <div className="group">
-      <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+      <div
+        className={`w-full aspect-w-1 aspect-h-1 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8
+        ${isLoading ? "bg-transparent" : "bg-gray-200"}`}
+      >
         <div className="h-60">
           {isLoading ? (
             <div className="w-full h-full bg-gray-400 animate-pulse"></div>
@@ -18,13 +21,7 @@ const AssetListItemView = ({ asset, handleClick, isLoading }) => {
       {isLoading ? (
         <div className="animate-pulse">
           <div className="mt-4 w-36 h-5 bg-gray-400 rounded-sm"></div>
-          {/* <h3 className="mt-4 text-sm text-gray-700 font-redacted tracking-tighter">
-            Loading name.............
-          </h3> */}
           <div className="mt-1 w-12 h-5 bg-gray-400 rounded-sm"></div>
-          {/* <p className="mt-1 text-lg font-medium text-gray-900 font-redacted tracking-tighter">
-            Price...
-          </p> */}
         </div>
       ) : (
         <div>
