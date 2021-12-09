6eeecf7229dd39996c83c5aa8f26bcdc2db15d32
Gnabriel <elleb@kth.se>
2021-12-09 15:30:13 +0100

    Fixed colors

--- src/components/pages/Login/LoginView.jsx
+++ src/components/pages/Login/LoginView.jsx
@@ -7,33 +7,31 @@ const LoginView = () => {
       <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
           <div>
-            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-black">
               Sign in to your account
             </h2>
-            <p className="mt-2 text-center text-sm text-gray-600">
-              Or{" "}
+            <p className="mt-2 text-center text-sm text-gray-dark">
+              New to Black White Club?{" "}
               <Link
-                to="/register"
-                className="font-medium text-gray-600 hover:text-indigo-500"
+                to="/signup"
+                className="font-medium text-gray-darker hover:text-gray"
               >
-                Register
+                Create an account
               </Link>
+              .
             </p>
           </div>
           <form className="mt-8 space-y-6" action="#" method="POST">
             <input type="hidden" name="remember" defaultValue="true" />
             <div className="rounded-md shadow-sm -space-y-px">
               <div>
-                <label htmlFor="email-address" className="sr-only">
-                  Email address
-                </label>
                 <input
                   id="email-address"
                   name="email"
                   type="email"
                   autoComplete="email"
                   required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-black focus:border-black focus:z-10 sm:text-sm"
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-light placeholder-gray text-gray-darker rounded-t-md focus:outline-none focus:ring-black focus:border-black focus:z-10 sm:text-sm"
                   placeholder="Email address"
                 />
               </div>
@@ -47,7 +45,7 @@ const LoginView = () => {
                   type="password"
                   autoComplete="current-password"
                   required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-black focus:border-black focus:z-10 sm:text-sm"
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-light placeholder-gray text-gray-darker rounded-b-md focus:outline-none focus:ring-black focus:border-black focus:z-10 sm:text-sm"
                   placeholder="Password"
                 />
               </div>
@@ -59,18 +57,18 @@ const LoginView = () => {
                   id="remember-me"
                   name="remember-me"
                   type="checkbox"
-                  className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                  className="h-4 w-4 text-black focus:ring-transparent border-gray rounded"
                 />
                 <label
                   htmlFor="remember-me"
-                  className="ml-2 block text-sm text-gray-900"
+                  className="ml-2 block text-sm text-gray-darker"
                 >
                   Remember me
                 </label>
               </div>
 
               <div className="text-sm">
-                <button className="font-medium text-black hover:text-gray-500">
+                <button className="font-medium text-black hover:text-gray">
                   Forgot your password?
                 </button>
               </div>
@@ -79,11 +77,11 @@ const LoginView = () => {
             <div>
               <button
                 type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-black hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-black hover:bg-gray-darker focus:outline-none focus:ring-none"
               >
                 <span className="absolute left-0 inset-y-0 flex items-center pl-3">
                   <LockClosedIcon
-                    className="h-5 w-5 text-black-500 group-hover:text-white"
+                    className="h-5 w-5 text-gray-darker group-hover:text-gray"
                     aria-hidden="true"
                   />
                 </span>
--- src/components/pages/Signup/SignupView.jsx
+++ src/components/pages/Signup/SignupView.jsx
@@ -1,4 +1,5 @@
 import { LockClosedIcon } from "@heroicons/react/solid";
+import { Link } from "react-router-dom";
 
 const SignupView = () => {
   return (
@@ -6,24 +7,31 @@ const SignupView = () => {
       <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
           <div>
-            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-              Register an account
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-black">
+              Become a member of the Black White Club!
             </h2>
+            <p className="mt-2 text-center text-sm text-gray-dark">
+              Already a member?{" "}
+              <Link
+                to="/signup"
+                className="font-medium text-gray-darker hover:text-gray"
+              >
+                Sign in to your account
+              </Link>
+              .
+            </p>
           </div>
           <form className="mt-8 space-y-6" action="#" method="POST">
             <input type="hidden" name="remember" defaultValue="true" />
             <div className="rounded-md shadow-sm -space-y-px">
               <div>
-                <label htmlFor="email-address" className="sr-only">
-                  Email address
-                </label>
                 <input
                   id="email-address"
                   name="email"
                   type="email"
                   autoComplete="email"
                   required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-black focus:border-black focus:z-10 sm:text-sm"
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-light placeholder-gray text-gray-darker rounded-t-md focus:outline-none focus:ring-black focus:border-black focus:z-10 sm:text-sm"
                   placeholder="Email address"
                 />
               </div>
@@ -37,7 +45,7 @@ const SignupView = () => {
                   type="password"
                   autoComplete="current-password"
                   required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-black focus:border-black focus:z-10 sm:text-sm"
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-light placeholder-gray text-gray-darker rounded-b-md focus:outline-none focus:ring-black focus:border-black focus:z-10 sm:text-sm"
                   placeholder="Password"
                 />
               </div>
@@ -49,13 +57,13 @@ const SignupView = () => {
                   id="remember-me"
                   name="remember-me"
                   type="checkbox"
-                  className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                  className="h-4 w-4 text-black focus:ring-transparent border-gray rounded"
                 />
                 <label
                   htmlFor="remember-me"
-                  className="ml-2 block text-sm text-gray-900"
+                  className="ml-2 block text-sm text-gray-darker"
                 >
-                  Remember me
+                  I agree to some GDPR stuff.
                 </label>
               </div>
             </div>
@@ -63,15 +71,15 @@ const SignupView = () => {
             <div>
               <button
                 type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-black hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-black hover:bg-gray-darker focus:outline-none focus:ring-none"
               >
                 <span className="absolute left-0 inset-y-0 flex items-center pl-3">
                   <LockClosedIcon
-                    className="h-5 w-5 text-black-500 group-hover:text-white"
+                    className="h-5 w-5 text-gray-darker group-hover:text-gray"
                     aria-hidden="true"
                   />
                 </span>
-                Register
+                Create account
               </button>
             </div>
           </form>
