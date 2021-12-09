7f633718ca28223854c66caf3655d4af139959a1
Gnabriel <elleb@kth.se>
2021-12-09 14:45:22 +0100

    Added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BrowsePresenter from "./components/pages/Browse/BrowsePresenter";
 import AssetDetailsPresenter from "./components/pages/AssetDetails/AssetDetailsPresenter";
 import HomeView from "./components/pages/Home/HomeView";
+import NavbarView from "./components/common/Navbar/NavbarView";
 
 function App() {
   return (
     <div className="selection:bg-gray text-black">
       <Router>
+        <NavbarView />
         <Routes>
           <Route path="/" element={<HomeView />} />
           <Route path="/browse" element={<BrowsePresenter />} />
