8ac027408faa658f3e3bc963cf8f0940a459d8b5
Gnabriel <elleb@kth.se>
2021-12-10 13:13:53 +0100

    Changed text, structure and colors

--- src/components/pages/Marketing/PostForm/PostFormView.jsx
+++ src/components/pages/Marketing/PostForm/PostFormView.jsx
@@ -6,101 +6,116 @@ const PostFormView = ({
   onPost,
 }) => {
   return (
-    <div className="mt-10 sm:mt-0">
-      <div className="md:grid md:grid-cols-3 md:gap-6">
-        <div className="md:col-span-1">
-          <div className="px-4 sm:px-0">
-            <h3 className="text-lg font-medium leading-6 text-gray-900">
-              Post a post
-            </h3>
-          </div>
+    <div className="md:grid md:grid-cols-3 md:gap-6">
+      <div className="md:col-span-1">
+        <div className="px-4 sm:px-0">
+          <h1 className="text-4xl font-extrabold tracking-tight text-black">
+            Submit a Twitter post
+          </h1>
+          <p className="mt-6 text-gray-dark">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <p className="mt-4 text-gray-dark">
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip. Excepteur!
+          </p>
         </div>
-        <div className="mt-5 md:mt-0 md:col-span-2">
-          <div className="shadow overflow-hidden sm:rounded-md">
-            <div className="px-4 py-5 bg-white sm:p-6">
-              <div className="grid grid-cols-6 gap-6">
-                <div className="col-span-6 sm:col-span-3">
-                  <label
-                    htmlFor="first-name"
-                    className="block text-sm font-medium text-gray-700"
-                  >
-                    Name
-                  </label>
-                  <input
+      </div>
+      <div className="mt-5 md:mt-0 md:col-span-2">
+        <div className="shadow overflow-hidden sm:rounded-md">
+          <div className="px-4 py-5 bg-white sm:p-6">
+            <div className="grid grid-cols-6 gap-6">
+              <div className="col-span-6 lg:col-span-3">
+                <label className="block text-sm font-medium text-gray-darker">
+                  Title
+                </label>
+                <input
+                  onInput={(e) => {
+                    onPostTitle(e.target.value);
+                  }}
+                  type="text"
+                  name="title"
+                  id="title"
+                  className="mt-1 focus:ring-black focus:border-black block w-full shadow-sm sm:text-sm border-gray-light rounded-md"
+                  placeholder="e.g 'My latest marketing post about Black White Club'"
+                />
+              </div>
+              <div className="col-span-6 lg:col-span-3">
+                <label
+                  htmlFor="input-username"
+                  className="block text-sm font-medium text-gray-darker"
+                >
+                  Your name
+                </label>
+                <input
+                  onInput={(e) => {
+                    onPostName(e.target.value);
+                  }}
+                  type="text"
+                  name="input-username"
+                  id="input-username"
+                  autoComplete="nickname"
+                  className="mt-1 focus:ring-black focus:border-black block w-full shadow-sm sm:text-sm border-gray-light rounded-md"
+                  placeholder="What should we call you?"
+                />
+              </div>
+              <div className="col-span-6 lg:col-span-3">
+                <label
+                  htmlFor="about"
+                  className="block text-sm font-medium text-gray-darker"
+                >
+                  Description
+                </label>
+                <div className="mt-1">
+                  <textarea
                     onInput={(e) => {
-                      onPostName(e.target.value);
+                      onPostText(e.target.value);
                     }}
-                    type="text"
-                    name="first-name"
-                    id="first-name"
-                    autoComplete="given-name"
-                    className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                    id="about"
+                    name="about"
+                    rows={3}
+                    className="shadow-sm focus:ring-black focus:border-black mt-1 block w-full sm:text-sm border border-gray-light rounded-md"
+                    placeholder="Describe the purpose of this marketing post."
+                    defaultValue={""}
                   />
                 </div>
-                <div>
-                  <label
-                    htmlFor="about"
-                    className="block text-sm font-medium text-gray-700"
-                  >
-                    Text
-                  </label>
-                  <div className="mt-1">
-                    <textarea
-                      onInput={(e) => {
-                        onPostText(e.target.value);
-                      }}
-                      id="about"
-                      name="about"
-                      rows={3}
-                      className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
-                      placeholder="What's on your mind?"
-                      defaultValue={""}
-                    />
-                  </div>
-                </div>
-                <div className="col-span-6 sm:col-span-3">
-                  <label className="block text-sm font-medium text-gray-700">
-                    Tweet URL
-                  </label>
+              </div>
+              <div className="col-span-6 lg:col-span-3">
+                <label
+                  htmlFor="tweet-url"
+                  className="block text-sm font-medium text-gray-darker"
+                >
+                  Tweet URL
+                </label>
+                <div className="mt-1 flex rounded-md shadow-sm">
+                  <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-light bg-gray-lighter text-gray text-sm">
+                    https://
+                  </span>
                   <input
-                    onInput={(e) => {
-                      onPostUrl(e.target.value);
-                    }}
                     type="text"
                     name="tweet-url"
                     id="tweet-url"
-                    autoComplete="given-name"
-                    className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                  />
-                </div>
-                <div className="col-span-6 sm:col-span-3">
-                  <label className="block text-sm font-medium text-gray-700">
-                    Title
-                  </label>
-                  <input
+                    className="focus:ring-black focus:border-black flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-light"
+                    placeholder="www.twitter.com/user/status/tweet-id"
                     onInput={(e) => {
-                      onPostTitle(e.target.value);
+                      onPostUrl(e.target.value);
                     }}
-                    type="text"
-                    name="title"
-                    id="title"
-                    autoComplete="given-name"
-                    className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                   />
                 </div>
               </div>
             </div>
-            <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
-              <button
-                onClick={(e) => {
-                  onPost();
-                }}
-                type="submit"
-                className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
-                Post
-              </button>
-            </div>
+          </div>
+          <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+            <button
+              onClick={(e) => {
+                onPost();
+              }}
+              type="submit"
+              className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-black hover:bg-gray-darker focus:outline-none"
+            >
+              Submit Tweet
+            </button>
           </div>
         </div>
       </div>
