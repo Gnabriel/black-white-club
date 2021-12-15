9c680bffb4c00c98f53967bd5578e385267495a0
Gnabriel <ls6l9g7g9ud1@opayq.com>
2021-12-15 23:51:47 +0100

    Changed key
    Co-authored-by: Emil Gustavsson <emigust@kth.se>

--- src/components/pages/Marketing/PostList/PostListView.jsx
+++ src/components/pages/Marketing/PostList/PostListView.jsx
@@ -4,7 +4,7 @@ const PostListView = ({ posts }) => {
   return (
     <div className="p-10 flex flex-col gap-5 items-center rounded-xl bg-gray-lighter">
       {posts.map((post) => {
-        return <PostView post={post} key={post.id} />;
+        return <PostView post={post} key={post.id + post.name + post.title} />;
       })}
     </div>
   );
