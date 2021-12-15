2ee46eb3f30003823b65d193dead3fbc40c75da3
Gnabriel <ls6l9g7g9ud1@opayq.com>
2021-12-15 22:03:23 +0100

    Fixed ability to sign up

--- src/components/pages/Signup/SignupPresenter.jsx
+++ src/components/pages/Signup/SignupPresenter.jsx
@@ -1,6 +1,45 @@
 import SignupView from "./SignupView";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
 
 const SignupPresenter = () => {
-  return <SignupView />;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [acceptGdpr, setAcceptGdpr] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [authError, setAuthError] = useState("");
+
+  const auth = getAuth();
+
+  const createUserWithEmail = () => {
+    setAuthError("");
+    acceptGdpr &&
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed in.
+          setIsAuthenticated(true);
+        })
+        .catch((error) => {
+          // Signed out.
+          if (error.code === "auth/email-already-in-use") {
+            setAuthError("This email already exists.");
+          }
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode + ": " + errorMessage);
+        });
+  };
+
+  return (
+    <SignupView
+      isAuthenticated={isAuthenticated}
+      setEmail={setEmail}
+      setPassword={setPassword}
+      acceptGdpr={acceptGdpr}
+      setAcceptGdpr={setAcceptGdpr}
+      handleClick={() => createUserWithEmail()}
+      authError={authError}
+    />
+  );
 };
 export default SignupPresenter;
