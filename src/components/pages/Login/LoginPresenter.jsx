39a1318c5052c97ef0e23a44721da28534bb078f
Gnabriel <ls6l9g7g9ud1@opayq.com>
2021-12-15 22:57:59 +0100

    Added login functionality
    Co-authored-by: Emil Gustavsson <emigust@kth.se>

--- src/components/pages/Login/LoginPresenter.jsx
+++ src/components/pages/Login/LoginPresenter.jsx
@@ -1,6 +1,50 @@
+import {
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import LoginView from "./LoginView";
+import { useState } from "react";
 
 const LoginPresenter = () => {
-  return <LoginView />;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [authError, setAuthError] = useState("");
+
+  const auth = getAuth();
+
+  const signInWithEmail = () => {
+    setAuthError("");
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        setIsAuthenticated(true);
+      })
+      .catch((error) => {
+        console.log(error);
+        setAuthError("Wrong username or password.");
+      });
+  };
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in.
+      setIsAuthenticated(true);
+    } else {
+      // User is signed out.
+      setIsAuthenticated(false);
+    }
+  });
+
+  return (
+    <LoginView
+      setEmail={setEmail}
+      setPassword={setPassword}
+      handleClick={() => signInWithEmail()}
+      isAuthenticated={isAuthenticated}
+      authError={authError}
+    />
+  );
 };
 export default LoginPresenter;
